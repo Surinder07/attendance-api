cb0564629ca6c972f064d7b762e6233ab3658194
Surinder0322 <surinder0322@gmail.com>
2022-10-29 19:51:02 -0400

    first commit

---
+++ src/main/java/com/example/studenthubapi/StudenthubApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.studenthubapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudenthubApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StudenthubApiApplication.class, args);
+    }
+
+}
